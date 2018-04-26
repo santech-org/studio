99e7d84ba6112a026ba6f57f8b2b6f01ff8c392d
Paul Souche <psouche@santech.fr>
2018-04-26 15:58:50 +0200

    feat(ci): add travis

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,16 +1,21 @@
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[", ]//g')
-PACKAGE_NAME=$(cat package.json \
-  | grep name \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
+for package in `find @santech -mindepth 1 -maxdepth 1 -type d`; do
+  cd "${package}"
+  PACKAGE_VERSION=$(cat package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[", ]//g')
+  PACKAGE_NAME=$(cat package.json \
+    | grep name \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g')
 
-PUBLISHED_PACKAGE_VERSIONS=`npm show ${PACKAGE_NAME} versions --json`
+  PUBLISHED_PACKAGE_VERSIONS=`npm show ${PACKAGE_NAME} versions --json`
 
-echo $PUBLISHED_PACKAGE_VERSIONS | grep "\"$PACKAGE_VERSION\"" > /dev/null \
-  && echo "Module Deja publié" \
-  || (npm publish dist.tgz --verbose --access=public)
+  echo $PUBLISHED_PACKAGE_VERSIONS | grep "\"$PACKAGE_VERSION\"" > /dev/null \
+    && echo "Module Deja publié ${package}" \
+    || (npm publish dist.tgz --verbose --access=public)
+  cd ../..
+done
+
