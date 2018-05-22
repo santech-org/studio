de84bbfeb8c81d51156a43beab3d2dc414f220fd
Paul Souche <psouche@santech.fr>
2018-05-22 14:02:21 +0200

    fix(platform): avoid naming collision even if suffixed

--- @santech/angular-platform/src/tokens/platform-global-context.token.ts
+++ @santech/angular-platform/src/tokens/platform-global-context.token.ts
@@ -1,4 +1,4 @@
 import { InjectionToken } from '@angular/core';
 import { platform } from '../interfaces/platform';
 
-export const PLATFORM_GLOBAL_CONTEXT = new InjectionToken<platform>('platform');
+export const PLATFORM_GLOBAL_CONTEXT = new InjectionToken<platform>('platformGlobalContext');
