7f4f4f954ae7ddea74011df16ec6ce5a1fd75f65
Paul Souche <psouche@santech.fr>
2018-06-27 18:06:08 +0200

    feat(testing): add spyLocation

--- @santech/angular-common/testing/index.ts
+++ @santech/angular-common/testing/index.ts
@@ -1,32 +1,37 @@
+import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { FileService, LoggerService } from '@santech/angular-common';
 import { createJestSpyObj, filterPrivate, SantechSpyObject } from '@santech/core/testing';
 
 export * from './mocks/timeout-mock';
 
-export const routerMethods = ['navigate'];
-
-export const loggerServiceMethods = Object
-  .getOwnPropertyNames(LoggerService.prototype).filter(filterPrivate);
 export const fileServiceMethods = Object
   .getOwnPropertyNames(FileService.prototype).filter(filterPrivate);
+export const locationMethods = ['back'];
+export const loggerServiceMethods = Object
+  .getOwnPropertyNames(LoggerService.prototype).filter(filterPrivate);
+export const routerMethods = ['navigate'];
 
+let spyFileService: SantechSpyObject<FileService>;
+let spyLocation: SantechSpyObject<Location>;
 let spyLoggerService: SantechSpyObject<LoggerService>;
 let spyRouter: SantechSpyObject<Router>;
-let spyFileService: SantechSpyObject<FileService>;
 
 if (typeof jasmine !== 'undefined' && typeof jasmine.createSpyObj === 'function') {
+  spyFileService = jasmine.createSpyObj('spyFileService', fileServiceMethods);
+  spyLocation = jasmine.createSpyObj('spyLocation', locationMethods);
   spyLoggerService = jasmine.createSpyObj('spyLoggerService', loggerServiceMethods);
   spyRouter = jasmine.createSpyObj('spyRouter', routerMethods);
-  spyFileService = jasmine.createSpyObj('spyFileService', fileServiceMethods);
 } else if (typeof jest !== 'undefined') {
+  spyFileService = createJestSpyObj(fileServiceMethods);
+  spyLocation = createJestSpyObj(locationMethods);
   spyLoggerService = createJestSpyObj(loggerServiceMethods);
   spyRouter = createJestSpyObj(routerMethods);
-  spyFileService = createJestSpyObj(fileServiceMethods);
 }
 
 export {
-  spyLoggerService,
-  spyRouter,
   spyFileService,
+  spyLocation,
+  spyRouter,
+  spyLoggerService,
 };
